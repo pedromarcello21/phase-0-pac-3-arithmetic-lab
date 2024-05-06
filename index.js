706676ec43f2f9923833443ee80b7e9cf64fb605
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-05-06 19:14:18 -0400

    calculator functions

---
+++ index.js
@@ -0,0 +1,31 @@
+const add = (x, y) => {
+    return x + y
+}
+
+const subtract = (x, y) => {
+    return x - y
+}
+
+const multiply = (x, y) => {
+    return x * y
+}
+
+const divide = (x, y) => {
+    return x / y
+}
+
+const increment = (x) => {
+    return ++x
+}
+
+const decrement = (x) => {
+    return --x
+}
+
+const makeInt = (x) => {
+    return parseInt(x, 10)
+}
+
+const preserveDecimal = (x) => {
+    return parseFloat(x)
+}
